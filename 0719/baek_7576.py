c908d15550fc5c7b86e16c0df736e00000534e02
nnnangni <yud02150@gmail.com>
2019-07-25 18:34:15 +0900

    serial number

--- 0719/baek_7576.py
+++ 0719/baek_7576.py
@@ -7,8 +7,10 @@ def bfs(a,b):
         ny = b + dy[i]
         if nx>=0 and ny>=0 and nx<N and ny<M:
             if arr[nx][ny]==0 and visited[nx][ny]==0:
+                # 날짜를 세어주는 대신 행렬 값을 바꾸기
                 arr[nx][ny] = arr[a][b]+1
                 visited[nx][ny]=1
+                # 큐에 새로 넣어주고 큐의 인덱스도 이동하는 작업
                 front += 1
                 queue[front] = nx
                 front += 1
@@ -17,7 +19,9 @@ def bfs(a,b):
 M,N = map(int, input().split())
 arr = []
 visited = []
+# 최대 크기의 큐 생성
 queue = [0]*M*N*2
+# 인덱스 값인 front와 rear 생성
 front = -1
 rear = -1
 for v in range(N):
@@ -27,16 +31,19 @@ for i in range(N):
 for p in range(N):
     for q in range(M):
         if arr[p][q] == 1:
+            # 1인 값을 모두 queue에 넣어주는 작업
             front += 1
             queue[front] = p
             front += 1
             queue[front] = q
 
+# pop 대신 rear을 활용해서 하나씩 값을 가져옴
 while front!=rear:
     rear += 1
     i = queue[rear]
     rear += 1
     j = queue[rear]
+    # 좌표를 가지고 함수 실행
     bfs(i,j)
 
 amax=0
